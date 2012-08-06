e0a7317a58a9383c9f7b9c6af4d8449d35106c12
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-08-06 13:54:57 +0100

    Additionally manage UTF_8 encoding in OSX app builds (fixes #30)

--- test/releasy/builders/data/Main.rb
+++ test/releasy/builders/data/Main.rb
@@ -6,7 +6,7 @@ OSX_EXECUTABLE_FOLDER = File.expand_path("../../..", __FILE__)
 
 # Really hacky fudge-fix for something oddly missing in the .app.
 class Encoding
-  BINARY = UTF_7 = UTF_16BE = UTF_16LE = UTF_32BE = UTF_32LE = Encoding.list.first
+  BINARY = UTF_8 = UTF_7 = UTF_16BE = UTF_16LE = UTF_32BE = UTF_32LE = Encoding.list.first
 end
 
 Dir.chdir 'application'
