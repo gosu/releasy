1598d115ea7bb26605f6aab546decee838b8aac8
Bil Bas (Spooner) <bil.bagpuss@gmail.com>
2012-08-09 13:40:24 +0100

    Oops, forgot to commit the altered hierarchy of exceptions.

--- lib/releasy.rb
+++ lib/releasy.rb
@@ -7,11 +7,13 @@ $LOAD_PATH.unshift File.expand_path("..", __FILE__)
 module Releasy
   def self.win_platform?; !!(RUBY_PLATFORM =~ /mingw|win32/); end
 
+  class ReleasyError < StandardError; end
+
   # An error caused when Releasy configuration is incorrect or inconsistent.
-  class ConfigError < StandardError; end
+  class ConfigError < ReleasyError; end
 
   # Failed to find a particular CLI command on the system.
-  class CommandNotFoundError < StandardError; end
+  class CommandNotFoundError < ReleasyError; end
 
   # Modules used to extend Releasy classes.
   module Mixins; end
